df4eebf863c913d30722c58d4b2fba7550ba4f5d
Paul Spencer <billsongames@gmail.com>
2023-01-23 22:02:03 +0000

    forecast summary built

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "../styles/App.css";
 import PropTypes from "prop-types";
 import LocationDetails from "./LocationDetails";
+import ForecastSummary from "./ForecastSummary";
 
 function App(props) {
   const { location } = props;
--- src/tests/components/LocationDetails.test.js
+++ src/tests/components/LocationDetails.test.js
@@ -1,3 +1,14 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import LocationDetails from '../components/LocationDetails';
+import LocationDetails from "../../components/LocationDetails";
+import ForecastSummary from "../../components/ForecastSummary";
+
+describe("LocationDetails", () => {
+  it("renders correct city and location props", () => {
+    const { getByText } = render(
+      <LocationDetails city="Manchester" country="UK" />
+    );
+
+    expect(getByText("Manchester, UK")).toBeInstanceOf(HTMLHeadingElement);
+  });
+});
